21c9732896e13dd9891cec0e883affb82a9d47aa
trungtlam15 <trungtlam15@gmail.com>
2022-08-06 22:15:24 -0500

    finish print value above and below primary diagonal

--- main.py
+++ main.py
@@ -16,9 +16,26 @@ def check_two_dimensional_array(twod_array):
                 max_value = twod_array[i][j]
     print("max row ",row_max,"max colum ",column_max, "max value ", max_value)
 
+def print_above_primary_diagonal(twod_array):
+    for i in range(len(twod_array)):
+        for j in range(len(twod_array[i])):
+            if j > i:
+                print(twod_array[i][j])
+
+def print_on_and_below_primary_diagonal(twod_array):
+    for i in range(len(twod_array)):
+        for j in range(len(twod_array[i])):
+            if j <= i:
+                print(twod_array[i][j])
+
+
 if __name__ == '__main__':
-    T = [[11, 12, 5, 2], [15, 6, 10], [10, 8, 12, 5], [12, 15, 8, 6]]
+    T = [[11, 12, 5, 2], [15, 6, 10, 7], [10, 8, 12, 5], [12, 15, 8, 6]]
+
     # print_two_dimensional_array(T)
 
-    check_two_dimensional_array(T)
+    # check_two_dimensional_array(T)
 
+    # print_above_primary_diagonal(T)
+
+    # print_on_and_below_primary_diagonal(T)
